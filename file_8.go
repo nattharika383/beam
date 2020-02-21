c4768b4579eedfd07c3c57426281c1083ae87bc7
nattharika383 <58070892+nattharika383@users.noreply.github.com>
2020-02-21 13:44:10 +0700

    Add files via upload

---
+++ file_8.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func walkFn(path string, info os.FileInfo, err error) error {
+	fmt.Println(path)
+	return nil
+}
+
+func main() {
+	filepath.Walk(".", walkFn)
+}
